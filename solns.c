570aa1c0a92b8048ddc5f7ef7cddef5c4552dd56
Usman370hub <85106921+Usman370hub@users.noreply.github.com>
2021-06-19 18:17:23 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,45 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+// Function for finding max
+
+int max(int arry [], int N ) {
+  
+  int maxx = arry[0];
+  for(int i = 0; i < N; i++){
+    if (arry[i] > maxx) {
+    maxx = arry[i];
+    }
+   } 
+   
+    return maxx;
+    
+ }     
+
+// Function for finding min
+
+int min(int arry [], int N ) {
+  
+  int minimum= arry[0];
+  for(int i = 0; i < N; i++){
+    if (arry[i] < minimum) {
+    minimum= arry[i];
+    }
+   } 
+   
+    return minimum;
+    
+ }
+
+
+// Function for finding average
+float average(int avg[], int N) {
+ int summation = 0;
+
+ for(int i = 0; i < N ; i++) {
+ summation += avg[i];
+    }
+     
+     float avrg = (float) summation/N ;
+     return avrg;
+     }
